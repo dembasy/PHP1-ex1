9c055e43cab3e6337578b459f1c5831dee658b43
dembasy <dembasy1227@gmail.com>
2018-07-25 14:06:52 +0200

    ex1 done

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>PHP ex1</title>
+  </head>
+  <body>
+  <?php
+  $name = "Demba";
+  echo $name;
+   ?>
+  </body>
+</html>
